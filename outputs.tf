a55844618d093ceaf5d1ca09945b26c717cd6fdb
Khadim Hussain Bahonar <khadimbahonar6@gmail.com>
2026-08-11 22:24:19 +0200

    Add public subnet

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "vpc_cidr_block" {
   description = "CIDR block of the VPC"
   value       = aws_vpc.main.cidr_block
 }
+
+output "public_subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public.id
+}
+
+output "public_subnet_cidr_block" {
+  description = "CIDR block of the public subnet"
+  value       = aws_subnet.public.cidr_block
+}
